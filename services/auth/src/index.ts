e1013737f0dd785f0a2b8e054cfb1b7c5c506039
kotkoroid <git@kotkoroid.com>
2025-04-21 11:56:25 +0200

    chore: integrate drizzle orm

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,6 +1,16 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
+import type { DrizzleD1Database } from 'drizzle-orm/d1';
+import { drizzle } from 'drizzle-orm/d1';
 
 export default class extends WorkerEntrypoint<AuthEnv> {
+	private database: DrizzleD1Database;
+
+	constructor(ctx: ExecutionContext, env: AuthEnv) {
+		super(ctx, env);
+
+		this.database = drizzle(this.env.DB_AUTH);
+	}
+
 	async fetch() {
 		return new Response('Hello from Auth service!');
 	}
